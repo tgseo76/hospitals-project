35f2341a7a1a5aa22d14242dac4961a88d256482
menuHwang <hmin0206@naver.com>
2022-11-16 21:48:39 +0900

    선행 연습

---
+++ src/main/java/com/team13/hospital/controller/HospitalController.java
@@ -0,0 +1,23 @@
+package com.team13.hospital.controller;
+
+import com.team13.hospital.domain.entity.Hospital;
+import com.team13.hospital.service.HospitalService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hospitals")
+public class HospitalController {
+    private final HospitalService hospitalService;
+
+    public HospitalController(HospitalService hospitalService) {
+        this.hospitalService = hospitalService;
+    }
+
+    @GetMapping("/{id}")
+    public Hospital findById(@PathVariable("id") Integer id) {
+        return hospitalService.findById(id);
+    }
+}
