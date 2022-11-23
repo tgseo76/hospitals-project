bace61f631c99f5746627ca5b53d2c836a572c72
menuHwang <hmin0206@naver.com>
2022-11-23 18:20:51 +0900

    member id로 조회 기능 구현

---
+++ src/main/java/com/team13/hospital/domain/entity/Member.java
@@ -0,0 +1,27 @@
+package com.team13.hospital.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private boolean admin;
+    private String name;
+    private int age;
+    private String phone;
+    private String address;
+}
---
+++ src/main/java/com/team13/hospital/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.team13.hospital.repository;
+
+import com.team13.hospital.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
